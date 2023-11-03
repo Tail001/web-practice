3bc8dbe5df11e49110232236dbb85cccbd50e0af
Chang <william104472@gmail.com>
2023-11-03 12:45:27 +0800

    lesson 10

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 const Home = () => {
+    // let name = 'unknown';
+    const [name, setName] = useState('known');
+    const [blogs, setBlogs] = useState([
+        { title: 'Breaking news' },
+        { title: 'what a surprising news!' },
+        { title: 'Web dev top tips' }
+    ]);
     const handleClick = () => {
-        console.log("hello this is home");
-    } 
+        setName('William');
+
+    }
 
     return (
         <div className="home">
+
             <h2>Home page</h2>
+            <p>{name}</p>
             <button onClick={handleClick}>click here</button>
         </div>
     )
