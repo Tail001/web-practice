3bee6a2c72bd0239c12f1b0ae43384590fdcf789
Chang <william104472@gmail.com>
2023-11-03 12:12:55 +0800

    lesson 7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const title = "Welcome to new blog";
+  const likes = 50;
+
   return (
     <div className="App">
+      <Navbar /> 
       <div className="content">
-        <h1>App component</h1>
+        <Home></Home>
+        <h1>App component: {title} </h1>
+        <h2> Likes: {likes} times</h2>
       </div>
     </div>
   );
