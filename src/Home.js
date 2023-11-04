1e765a1181475d3c985646c689b7394fc56538d7
Chang <william104472@gmail.com>
2023-11-04 23:22:32 +0800

    finish lesson 10

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,10 @@ const Home = () => {
     // let name = 'unknown';
     const [name, setName] = useState('known');
     const [blogs, setBlogs] = useState([
-        { title: 'Breaking news' },
-        { title: 'what a surprising news!' },
-        { title: 'Web dev top tips' }
+        { title: 'Breaking news', id: 0, content: 'pesudo content' },
+        { title: 'what a surprising news!', id: 1, content: 'pesudo content' },
+        { title: 'Web dev top tips', id: 2, content: 'NULL' },
+        { title: 'next js 14 release', id: 3, content: 'Null' }
     ]);
     const handleClick = () => {
         setName('William');
@@ -15,7 +16,13 @@ const Home = () => {
 
     return (
         <div className="home">
+            {blogs.map((blog) =>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>contentt: {blog.content}</p>
+                </div>
 
+            ))}
             <h2>Home page</h2>
             <p>{name}</p>
             <button onClick={handleClick}>click here</button>
